7925495ce25da35c662f9ae541501e446620a551
Jhesabel <201803483@est.umss.edu>
2025-09-30 08:51:03 -0400

    git push origin mainRevert "Merge pull request #1 from BiancaFernandezQ/CarolinaAlcocer"
    This reverts commit 974fecf556a73f7302afcf665b1c964f9c31fce2, reversing
    changes made to 4859b8589ba67fbe6f116dcefc1fa653e037ad6c.

--- fixtures/trello_fixture.js
+++
@@ -1,9 +0,0 @@
-import { test as base } from '@playwright/test';
-import { TrelloAPI } from '../api/trello_api.js';
-
-export const test = base.extend({
-  trello: async ({ request }, use) => {
-    const trello = new TrelloAPI(request);
-    await use(trello);
-  },
-});
