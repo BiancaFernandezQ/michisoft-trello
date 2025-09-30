54f1bde8dfe6b8ae14f0bb3e20d3baf38b01e595
CarolinaAVar <carocav081@gmail.com>
2025-09-29 22:26:09 -0400

    "Base y primer commit CAV"

---
+++ pages/login.page.js
@@ -0,0 +1,20 @@
+export class LoginPage {
+  constructor(page) {
+    this.page = page;
+    this.usernameInput = page.locator('input[placeholder="Enter your email"]');
+    this.continueButton = page.getByRole('button', { name: /Continue/i });
+    this.passwordInput = page.locator('#password');
+    this.loginButton = page.getByTestId('login-submit-idf-testid');
+  }
+
+  async goToLogin() {
+    await this.page.goto('https://trello.com/login');
+  }
+
+  async login(username, password) {
+    await this.usernameInput.fill(username);
+    await this.continueButton.click();
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+}
