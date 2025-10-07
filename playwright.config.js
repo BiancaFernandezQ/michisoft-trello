7803676f8c5083f682ca1196f80fb340a009bd0c
GabyRebound <gabriela.nataly.torrico@gmail.com>
2025-10-07 18:49:10 -0400

    correcion playwright

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
     testDir: 'tests',
 
-
+//allure
     reporter: [
         ['html'], 
         ['allure-playwright']
